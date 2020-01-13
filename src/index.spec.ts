5d0c150f21aba61be392527da01eebcbf0dc20b6
TJ Kandala <kandalatj@gmail.com>
2020-01-13 18:01:30 -0500

    gitattributes

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,7 @@
 import createObsducer from "./index";
 import { map } from "rxjs/operators";
 
+// testing interface/functionality, not implementation details
 test("create obsducer", () => {
   expect(
     createObsducer(map((x: number) => x + 1))([1, 2, 3, 4, 5])
